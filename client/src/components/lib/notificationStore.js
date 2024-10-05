85fc3c81eee217ab224410354a0342b52f97f599
chayma550 <rachayma@gmail.com>
2024-10-05 19:19:43 +0200

    solve problem

--- client/src/components/lib/notificationStore.js
+++ client/src/components/lib/notificationStore.js
@@ -1,19 +1,16 @@
-import React, { useContext, useEffect } from 'react';
-import { AuthContext } from '../../Context/AuthContext';
-import { useNotificationStore } from '../lib/notificationStore';
+import { create } from "zustand";
+import apiRequest from "./apiRequest";
 
-const NotificationComponent = () => {
-  const { currentUser } = useContext(AuthContext);
-  const fetchNotifications = useNotificationStore((state) => state.fetch);
-
-  useEffect(() => {
-    if (currentUser) {
-      fetchNotifications(currentUser.accessToken);
-    }
-  }, [currentUser, fetchNotifications]);
-
-  // Render your component based on notifications
-  return <div>Notifications: {useNotificationStore((state) => state.number)}</div>;
-};
-
-export default NotificationComponent;
+export const useNotificationStore = create((set) => ({
+  number: 0,
+  fetch: async () => {
+    const res = await apiRequest("/users/notification");
+    set({ number: res.data });
+  },
+  decrease: () => {
+    set((prev) => ({ number: prev.number - 1 }));
+  },
+  reset: () => {
+    set({ number: 0 });
+  },
+}));
