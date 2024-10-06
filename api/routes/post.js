c8de9cba678f572ca4bdf37715caf2ceb237d012
chayma550 <rachayma@gmail.com>
2024-10-06 21:00:23 +0200

    token updated

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/Post.js";
-import { verifyToken } from "../utils/VerifyToken.js";
 
 const router=express.Router()
 router.get("/",getPosts)
